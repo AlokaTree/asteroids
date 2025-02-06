94899b17955375914987bb605d1a415b148718f7
AlokaTree <p_lemaistre@hotmail.com>
2025-02-06 20:06:57 +0000

    player rotation working

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     print("Starting asteroids!")
@@ -15,8 +17,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
